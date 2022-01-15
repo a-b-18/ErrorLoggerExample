12ab56a7668adf63dad852ac39257292851d5db7
Alex Beazley <alexbeazley18@outlook.com>
2022-01-15 14:59:06 +1100

    accepts string array

--- Program.cs
+++ Program.cs
@@ -1,31 +1,43 @@
 ﻿
 namespace Sample_Cs
 {
-    public class Person
+    public class PersonReview
     {
-        public readonly bool HasErrors = false;
+        private readonly string _fileName;
+        private readonly int _fileLine;
+        public readonly bool FoundErrors = false;
         public readonly List<ErrorLog> Errors = new List<ErrorLog>();
 
-        public Person(string name, int age)
+        public PersonReview(string[] entries, string fileName, int fileLine)
         {
+            // Assign logging variables
+            _fileName = fileName;
+            _fileLine = fileLine;
+            
+            // Assign check-specific variables
+            string name = entries[0];
+            int age = Convert.ToInt16(entries[1]);
+            
+            // Call checks to be done
             CheckName(name);
             CheckAge(age);
             
-            if (Errors.Count > 0) { HasErrors = true; }
+            // Check if errors were logged
+            if (Errors.Count > 0) { FoundErrors = true; }
         }
 
         private void CheckName(string name)
         {
             if (name != "Tony") return;
             
-            Errors.Add(new ErrorLog {ErrorMessage = $"hah, {name} is a gay name.", IsCritical = false});
+            Errors.Add(new ErrorLog {ErrorMessage = $"file {_fileName} on line {_fileLine} has {name}, which is a bad name.", IsCritical = false});
         }
 
         private void CheckAge(int age)
         {
             if (age <= 30) return;
             
-            Errors.Add(new ErrorLog {ErrorMessage = $"{age} is way too old.", IsCritical = true});
+            Errors.Add(new ErrorLog {ErrorMessage = $"file {_fileName} on line {_fileLine} has {age}, which way too old.", IsCritical = true});
         }
         
     }
@@ -40,25 +52,38 @@ namespace Sample_Cs
     {
         public static void Main()
         {
-
-            var person = new Person("Alex", 31);
-
-            if (person.HasErrors)
+            string fileName = "theFileXXX//wat//";
+            
+            List<string[]> personList = new List<string[]>()
             {
-                foreach (var errorLog in person.Errors)
+                new string[] {"Alex", "29"},
+                new string[] {"Tony", "11"},
+                new string[] {"Fred", "31"},
+                new string[] {"Bud", "1"},
+            };
+
+            for (var i = 0; i < personList.Count; i++)
+            {
+                var person = personList[i];
+                
+                var personReview = new PersonReview(person, fileName, i + 1);
+                
+                if (personReview.FoundErrors)
                 {
-                    switch (errorLog.IsCritical)
+                    foreach (var errorLog in personReview.Errors)
                     {
-                        case true:
-                            Console.WriteLine("CRITICAL error: " + errorLog.ErrorMessage);
-                            break;
-                        case false:
-                            Console.WriteLine("Non-critical error: " + errorLog.ErrorMessage);
-                            break;
+                        switch (errorLog.IsCritical)
+                        {
+                            case true:
+                                Console.WriteLine("CRITICAL error: " + errorLog.ErrorMessage);
+                                break;
+                            case false:
+                                Console.WriteLine("Non-critical error: " + errorLog.ErrorMessage);
+                                break;
+                        }
                     }
                 }
             }
-
         }
     }
 }
